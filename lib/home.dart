40a0a5dc4741e9afcb611f5ea004ffcd19c84c77
Amit Singh <amit91823@gmail.com>
2021-01-27 15:33:57 +0530

    Drawer Added

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:newapp/main.dart';
 
 class NewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text('SabjiWaleBhiya'),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [Text("hello")],
+            ),
           ),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -15,15 +20,27 @@ class NewApp extends StatelessWidget {
               Card(
                 color: Colors.green,
                 child: Container(
-                  width: 80,
-                  height: 80,
-                  child: Text('CLOSED!'),
+                  width: 100,
+                  height: 60,
+                  alignment: Alignment.center,
+                  child: Text('CLOSED!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 25)),
                 ),
               ),
               Card(
-                color: Colors.orange,
                 child: Text(
-                    'Cart is closed due to unforeseen circumstances.We regret the inconvenience caused.We will notify you when the cart respens or plese check tomorrow between 8:00Am- 9:00 AM'),
+                  'Cart is closed due to unforeseen circumstances.We regret the inconvenience caused.We will notify you when the cart respens or plese check tomorrow between',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Container(
+                child: Text(
+                  " 8:00Am- 9:00 AM",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
               )
             ],
           )),
